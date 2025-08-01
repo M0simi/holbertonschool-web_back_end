bf4db3345725a415e49171a28a9298a9ccbecd66
Your Name <you@example.com>
2025-08-01 16:16:30 +0300

    edit

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-"""Collect 10 random numbers using async comprehension"""
+"""This module defines an asynchronous coroutine that collects 10 random
+numbers using an async comprehension."""
+
 
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
+
+async_generator = __import__("0-async_generator").async_generator
+
 
 async def async_comprehension() -> List[float]:
     """
