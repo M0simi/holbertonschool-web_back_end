cca21b790c73bd6d030cfbc0936553e20bd9e731
Your Name <you@example.com>
2025-08-17 23:29:46 +0300

    edit task12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""
+Script to display statistics about Nginx logs stored in MongoDB.
+"""
+
 from pymongo import MongoClient
 
-
-def helper(a: dict) -> int:
-    """return log"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs = client.logs.nginx
-    return logs.count_documents(a)
-
-
-def main():
-    """ provides some stats about Nginx logs stored in MongoDB """
-    print(f"{helper({})} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {helper({'method': 'GET'})}")
-    print(f"\tmethod POST: {helper({'method': 'POST'})}")
-    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
-    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
-
-
 if __name__ == "__main__":
-    main()
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+
+    # total of logs
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods stats
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Status check
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
