eb3684bc0a8e57774546efcd5b2bf35a016e5005
Your Name <you@example.com>
2025-08-23 19:42:01 +0300

    task 3

---
+++ ES6_basic/eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": "warn",
+    },
+  },
+];
+
