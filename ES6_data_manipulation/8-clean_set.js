a33dd4e50253eacf46b99b011cee91201a17abbe
Your Name <you@example.com>
2025-08-27 20:49:14 +0300

    edit

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(mySet, startString) {
-  if (!startString) return '';
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
 
   return Array.from(mySet)
     .filter((value) => value.startsWith(startString))
