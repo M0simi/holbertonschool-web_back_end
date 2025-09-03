62dc080c86a857fc9e9cc5269d681d1652d4ff23
Your Name <you@example.com>
2025-09-03 20:07:52 +0300

    edit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf-8');
 // listen to the input of the user
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // trim remove any new line or spaces
-  console.log(`Your name is: ${name}`);
+  const name = process.stdout.read(); // trim remove any new line or spaces
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 // listen to the end of stdin
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
